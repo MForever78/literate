b37af64aba1a5f68c326c04a8fe1dd0dcbf05a2f
MForever78 <shunjian1128@gmail.com>
2016-10-05 17:46:27 +0800

    feat: Add updateMiniBatch function

--- src/network.cpp
+++ src/network.cpp
@@ -2,6 +2,7 @@
 #define NETWORK_HPP
 
 #include "network.hpp"
+#include <algorithm>
 #include <random>
 
 using namespace std;
@@ -42,6 +43,41 @@ void Network::sgd(vector<trainingData> &trainingSet, const int epochs,
 }
 
 void Network::updateMiniBatch(std::vector<trainingData *> &miniBatch,
-                              const double eta) {}
+                              const double eta) {
+  vector<vec> partialB(biasesShape);
+  vector<mat> partialW(weightsShape);
+
+  for (auto trainingNode : miniBatch) {
+    vector<vec> deltaPartialB(biasesShape);
+    vector<mat> deltaPartialW(weightsShape);
+
+    backprop(trainingNode->first, trainingNode->second, deltaPartialB,
+             deltaPartialW);
+
+    for (int i = 0; i < partialB.size(); ++i) {
+      partialB[i] = partialB[i] + deltaPartialB[i];
+    }
+    for (int i = 0; i < partialW.size(); ++i) {
+      partialW[i] = partialW[i] + deltaPartialW[i];
+    }
+  }
+
+  for_each(partialB.begin(), partialB.end(), [&](vec &bias) {
+    bias.transform([&](double b) { return eta / miniBatch.size() * b; });
+  });
+  for_each(partialW.begin(), partialW.end(), [&](mat &weight) {
+    weight.transform([&](double w) { return eta / miniBatch.size() * w; });
+  });
+
+  for (int i = 0; i < biases.size(); ++i) {
+    biases[i] = biases[i] - partialB[i];
+  }
+  for (int i = 0; i < weights.size(); ++i) {
+    weights[i] = weights[i] - partialW[i];
+  }
+}
+
+void Network::backprop(vec &in, vec &out, vector<vec> &partialB,
+                       vector<mat> &partialW) {}
 
 #endif
--- src/network.hpp
+++ src/network.hpp
@@ -2,7 +2,7 @@
 #include <cmath>
 #include <vector>
 
-typedef std::pair<arma::mat, int> trainingData;
+typedef std::pair<arma::vec, arma::vec> trainingData;
 
 class Network {
 public:
@@ -11,10 +11,13 @@ public:
 
     for (int i = 1; i < sizes.size(); ++i) {
       biases.push_back(arma::vec(sizes[i], arma::fill::randn));
+      biasesShape.push_back(arma::vec(sizes[i], arma::fill::zeros));
     }
 
     for (int i = 0; i < sizes.size() - 1; ++i) {
       weights.push_back(arma::mat(sizes[i], sizes[i + 1], arma::fill::randn));
+      weightsShape.push_back(
+          arma::mat(sizes[i], sizes[i + 1], arma::fill::zeros));
     }
   }
 
@@ -27,6 +30,8 @@ public:
            const int miniBatchSize, const double eta);
   void updateMiniBatch(std::vector<trainingData *> &miniBatch,
                        const double eta);
+  void backprop(arma::vec &in, arma::vec &out, std::vector<arma::vec> &partialB,
+                std::vector<arma::mat> &partialW);
 
 private:
   Network(){};
@@ -34,7 +39,7 @@ private:
   int layerNumber;
   std::vector<int> sizes;
   // biases[i] represents bias at (i+1)th layer
-  std::vector<arma::vec> biases;
+  std::vector<arma::vec> biases, biasesShape;
   // weights[i] represents weight at ith layer
-  std::vector<arma::mat> weights;
+  std::vector<arma::mat> weights, weightsShape;
 };
